9bbb126b5ee24c76e86e49495141d6d557a204e6
nickfarrow <nick@nickfarrow.com>
2022-11-15 22:56:10 +1100

    Separate lnd.rs

---
+++ src/lnd.rs
@@ -0,0 +1,68 @@
+use configparser::ini::Ini;
+use tonic_lnd::lnrpc::AddInvoiceResponse;
+
+pub fn load_conf() -> (String, String, String, String) {
+    let mut config = Ini::new();
+    let _map = config
+        .load("./config.cfg")
+        .expect("config.cfg does not exist! please copy config_example.cfg");
+
+    let address = config.get("lnd", "address").expect("address provided");
+    let cert = config.get("lnd", "certfile").expect("cert provided");
+    let macaroon = config
+        .get("lnd", "macaroonfile")
+        .expect("macaroon provided");
+    let label = config
+        .get("lnd", "defaultlabel")
+        .expect("default label provided");
+    (address, cert, macaroon, label)
+}
+
+pub async fn create_invoice(amount: i64, description: String) -> AddInvoiceResponse {
+    let (address, cert, macaroon, _) = load_conf();
+    let mut client = tonic_lnd::connect_lightning(address, cert, macaroon)
+        .await
+        .expect("failed to connect");
+
+    // let sat_amount = amount.checked_mul(1000).expect("not billions(?) of sats") as i64;
+
+    let invoice = tonic_lnd::lnrpc::Invoice {
+        memo: description,
+        value: amount,
+        ..Default::default()
+    };
+
+    let created_invoice = client
+        .add_invoice(invoice)
+        .await
+        // , &payment_id.to_string(), &description, None)
+        .expect(
+            "created invoice with lightning node using ssh tunnel!\n
+             Run `ssh pi@your.node.ip -q -N -L 10009:localhost:10009`\n\n",
+            // Run `ssh -nNT -L ./lightning-rpc:/root/.lightning/bitcoin/lightning-rpc user@host`\n\n"
+        )
+        .into_inner();
+
+    created_invoice
+}
+
+pub async fn check_invoice(payment_id: String) -> bool {
+    let (address, cert, macaroon, _label) = load_conf();
+    let mut client = tonic_lnd::connect_lightning(address, cert, macaroon)
+        .await
+        .expect("failed to connect");
+
+    let payment_hash = tonic_lnd::lnrpc::PaymentHash {
+        r_hash: hex::decode(payment_id).expect("valid payment hash"),
+        ..Default::default()
+    };
+
+    let invoice = client
+        .lookup_invoice(payment_hash)
+        .await
+        .expect("fetched invoices")
+        .into_inner();
+
+    let payment_complete = if invoice.state == 1 { true } else { false };
+    payment_complete
+}
--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,18 @@
 extern crate rocket;
 
 pub mod cashu;
+pub mod lnd;
 pub mod wildcard;
 use std::sync::Mutex;
 
-// use clightningrpc::LightningRPC;
 use qrcode_generator::QrCodeEcc;
 use rocket::{fs::FileServer, http::Status, serde::json::Json};
 use sateater::{
     cashu::cashu_receive,
-    load_conf,
     vault::{board, land, BattleConfig},
     BOARD_SIZE,
 };
 use serde::Serialize;
-
-// const RPC_FILE: &str = "lightning-rpc";
-
-// // Database not required
-// use rocket_db_pools::{
-//     sqlx::{self, Sqlite, Transaction},
-//     Database,
-// };
-// #[derive(Database)]
-// #[database("main")]
-// pub struct Main(sqlx::SqlitePool);
 
 #[derive(Serialize, Debug)]
 pub struct PaymentResponse {
@@ -40,37 +28,13 @@ pub async fn create_payment(
     amount: i64,
     message: Option<String>,
 ) -> (Status, Json<PaymentResponse>) {
-    // let node = LightningRPC::new(RPC_FILE);
-    // let payment_id = Uuid::new_v4();
-    let (address, cert, macaroon, label) = load_conf();
-    let mut client = tonic_lnd::connect_lightning(address, cert, macaroon)
-        .await
-        .expect("failed to connect");
-
+    let (_, _, _, label) = lnd::load_conf();
     let description = match message {
         Some(message) => message,
         None => label,
     };
 
-    // let sat_amount = amount.checked_mul(1000).expect("not billions(?) of sats") as i64;
-
-    let invoice = tonic_lnd::lnrpc::Invoice {
-        memo: description,
-        value: amount,
-        ..Default::default()
-    };
-
-    let created_invoice = client
-        .add_invoice(invoice)
-        .await
-        // , &payment_id.to_string(), &description, None)
-        .expect(
-            "created invoice with lightning node using ssh tunnel!\n
-             Run `ssh pi@your.node.ip -q -N -L 10009:localhost:10009`\n\n",
-            // Run `ssh -nNT -L ./lightning-rpc:/root/.lightning/bitcoin/lightning-rpc user@host`\n\n"
-        )
-        .into_inner();
-
+    let created_invoice = lnd::create_invoice(amount, description).await;
     let payment_id = hex::encode(created_invoice.r_hash);
     create_qr_code(
         created_invoice.payment_request.clone(),
@@ -107,26 +71,8 @@ pub struct PaymentStatusResponse {
 
 #[get("/check_payment?<payment_id>")]
 pub async fn check_payment(payment_id: String) -> (Status, Json<PaymentStatusResponse>) {
-    let (address, cert, macaroon, _label) = load_conf();
-    let mut client = tonic_lnd::connect_lightning(address, cert, macaroon)
-        .await
-        .expect("failed to connect");
-
-    let payment_hash = tonic_lnd::lnrpc::PaymentHash {
-        r_hash: hex::decode(payment_id).expect("valid payment hash"),
-        ..Default::default()
-    };
-
-    let invoice = client
-        .lookup_invoice(payment_hash)
-        .await
-        .expect("fetched invoices")
-        .into_inner();
-
-    let payment_complete = if invoice.state == 1 { true } else { false };
-
     let response = PaymentStatusResponse {
-        payment_complete: payment_complete,
+        payment_complete: lnd::check_invoice(payment_id).await,
         // For later doing onchain
         confirmed_paid: 0,
         unconfirmed_paid: 0,
